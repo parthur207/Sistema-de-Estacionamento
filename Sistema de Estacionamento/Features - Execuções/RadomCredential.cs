6654a43425d35e56d3d69cbc54ac3459bff7f8bf
Paulo Andrade <parthur207@gmail.com>
2024-10-21 17:59:45 -0300

    ok

--- Sistema de Estacionamento/Features - Execuções/RadomCredential.cs
+++ Sistema de Estacionamento/Features - Execuções/RadomCredential.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sistema_de_Estacionamento.IFeatures;
+using Sistema_de_Estacionamento.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_Estacionamento.Features___Execuções
 {
-    internal class RadomCredential
+    internal class RandomCredential : StorageClient, IFeature_Parking
     {
+        public string[] C_Radom()
+        {
+            string[] Credencial = new string[6];
+            bool random = true;
+
+            while (random)
+            {
+
+                Random Letter = new Random();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    char letraAleatoria = (char)Letter.Next(65, 91);//ASCII
+                    Credencial[i] = letraAleatoria.ToString();
+                }
+                Random Number = new Random();
+                for (int i = 3; i < Credencial.Length; i++)
+                {
+                    int numeroAleatorio = Number.Next(0, 10);// 0 A 9
+                    Credencial[i] = numeroAleatorio.ToString();
+                }
+
+                //Incremento de método que irá verificar se ja existe alguma credencial parecida com a gerada, se sim, será realizado uma nova
+                if ()
+                {
+
+                }
+                else
+                {
+                    random = false;
+                }
+            }
+            return Credencial;
+        }
     }
 }
