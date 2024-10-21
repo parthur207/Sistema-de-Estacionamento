9ad0211926c7625f99a7c4fb3fb0c11700ff333a
Paulo Andrade <parthur207@gmail.com>
2024-10-21 17:58:44 -0300

    ok

--- Sistema de Estacionamento/Features - Execuções/FinalValue.cs
+++ Sistema de Estacionamento/Features - Execuções/FinalValue.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sistema_de_Estacionamento.IFeatures;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_Estacionamento.Features___Execuções
 {
-    internal class FinalValue
+    internal class FinalValue : IFeature_Parking
     {
+        protected static double Preco_Minuto { get; set; } = 0.10;
+        public double Pagamento(TimeSpan Periodo)
+        {
+            double Preco;
+            // Calculo padrão do valor a ser pago: VALOR A PAGAR = (0.10 * minutos)
+            if (Periodo.TotalMinutes >= 480) //480 minutos, equivalente a 8 horas
+            {
+                Preco = 50.00;
+            }
+            else
+            {
+                Preco = Preco_Minuto * Periodo.TotalMinutes;
+            }
+            return Preco;
+        }
     }
 }
