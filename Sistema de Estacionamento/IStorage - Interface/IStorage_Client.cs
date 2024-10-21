97be4c38ca028b232b0a62ade6de7cf0151a4db0
Paulo Andrade <parthur207@gmail.com>
2024-10-21 17:55:22 -0300

    ok

---
+++ Sistema de Estacionamento/IStorage - Interface/IStorage_Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Estacionamento.IStorage___Interface
+{
+    internal interface IStorage_Client
+    {
+    }
+}
