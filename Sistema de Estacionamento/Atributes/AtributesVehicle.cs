8fb334c5c42d2730d271860aed7b41a6ec0a7108
Paulo Andrade <parthur207@gmail.com>
2024-10-21 17:50:20 -0300

    ok

--- Sistema de Estacionamento/Atributes/AtributesVehicle.cs
+++ Sistema de Estacionamento/Atributes/AtributesVehicle.cs
@@ -8,5 +8,16 @@ namespace Sistema_de_Estacionamento.Atributes
 {
     internal class AtributesVehicle
     {
+        public Tipo_Veiculo TipoVeiculo { get; set; }
+        public string Nome_Veiculo { get; set; }
+        public string Cor { get; set; }
+        public string Placa { get; set; }
+
+    }
+    public enum Tipo_Veiculo
+    {
+        Caminhão,
+        Carro,
+        Moto
     }
 }
