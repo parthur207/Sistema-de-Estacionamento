39aad28434a1b0823a7c31301426a2e883f09b2e
Paulo Andrade <parthur207@gmail.com>
2024-10-21 17:53:09 -0300

    ok

---
+++ Sistema de Estacionamento/Features - Execuções/FinalValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Estacionamento.Features___Execuções
+{
+    internal class FinalValue
+    {
+    }
+}
