fb72bea6553f0a237f2688c56cd0be8ee95b89ef
Paulo Andrade <parthur207@gmail.com>
2024-10-21 17:59:01 -0300

    ok

--- Sistema de Estacionamento/Features - Execuções/QueryVenancies.cs
+++ Sistema de Estacionamento/Features - Execuções/QueryVenancies.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sistema_de_Estacionamento.System___Config;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_Estacionamento.Features___Execuções
 {
-    internal class QueryVenancies
+    internal class QueryVenancies : CarTruck_Parking, IExecution
     {
+        public QueryVenancies(int numeroVagas) : base(numeroVagas) { }
+
+
+
     }
 }
