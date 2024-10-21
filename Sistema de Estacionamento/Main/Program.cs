c43b3861c57d51ddd9ceb16d32a1bd11100b0e38
Paulo Andrade <parthur207@gmail.com>
2024-10-21 17:43:43 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema de Estacionamento/Main/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Sistema_de_Estacionamento.Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
