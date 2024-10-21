c4d21477d36e9e82f2685537f1cf1a52501b8337
Paulo Andrade <parthur207@gmail.com>
2024-10-21 17:49:52 -0300

    ok

--- Sistema de Estacionamento/Atributes/AtributesClient.cs
+++ Sistema de Estacionamento/Atributes/AtributesClient.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_Estacionamento.Atributes
 {
-    internal class AtributesClient
+    internal class AtributesClient : AtributesVehicle
     {
+        public static string[] ref_args { get; private set; }//Parâmetro de referência ao método Main
+
+        protected string Credencial_Acesso { get; set; }
+        protected string Nome { get; set; }
+        protected DateTime Entrada { get; set; }
+        protected DateTime Saida { get; set; }
+        protected DateTime Periodo { get; set; }
     }
 }
