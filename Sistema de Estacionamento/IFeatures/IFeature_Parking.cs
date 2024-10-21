0807280787fd3612657326c7c6f08076b5aed6b7
Paulo Andrade <parthur207@gmail.com>
2024-10-21 18:00:37 -0300

    .

--- Sistema de Estacionamento/IFeatures/IFeature_Parking.cs
+++ Sistema de Estacionamento/IFeatures/IFeature_Parking.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sistema_de_Estacionamento.Atributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace Sistema_de_Estacionamento.IFeatures
 {
     internal interface IFeature_Parking
     {
+        TimeSpan CheckOut(DateTime Entrada, DateTime Saida)
+        {
+            TimeSpan Periodo_Estacionamento = Entrada - Saida;
+            return Periodo_Estacionamento;
+        }
+
+        void Exibition_Venancies(Tipo_Veiculo veiculo) { }
+
+        void C_Radom() { }
+
+        decimal Pagamento() { return 0; }
     }
 }
