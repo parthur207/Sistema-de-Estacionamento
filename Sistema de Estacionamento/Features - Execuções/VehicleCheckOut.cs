bb4a699ba1ca7e78f87e543dbf87208906f25e74
Paulo Andrade <parthur207@gmail.com>
2024-10-21 18:00:07 -0300

    .

--- Sistema de Estacionamento/Features - Execuções/VehicleCheckOut.cs
+++ Sistema de Estacionamento/Features - Execuções/VehicleCheckOut.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sistema_de_Estacionamento.IFeatures;
+using Sistema_de_Estacionamento.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_Estacionamento.Features___Execuções
 {
-    internal class VehicleCheckOut
+    internal class VehicleCheckOut : StorageClient, IFeature_Parking
     {
+        protected TimeSpan PeriodoEstadia { get; private set; }
+        public TimeSpan CheckOut(DateTime Inicio, DateTime Final)
+        {
+            return PeriodoEstadia;
+        }
     }
 }
