26653da3dbf49092f585bee3069a824cbe8010f7
Paulo Andrade <parthur207@gmail.com>
2024-10-29 17:27:39 -0300

    ok

--- Sistema de Estacionamento/Atributes/AtributesParking.cs
+++ Sistema de Estacionamento/Atributes/AtributesParking.cs
@@ -7,50 +7,22 @@ using System.Threading.Tasks;
 
 namespace Sistema_de_Estacionamento.Atributes
 {
-<<<<<<< HEAD
-    [Table("Estacionamento")]
-    internal abstract class AttributesParking
-    {
-        protected abstract int NumeroVagas { get; set; }
-
-        protected abstract Tipo_Veiculo TipoVeiculo { get; set; }
-
-=======
-    [Table("Tabela_Estacionamento")]
+    
     internal abstract class AttributesParking
     {
         protected abstract int NumeroVagas { get; set; }
         protected abstract List<bool> Vagas { get; set; }
-        
->>>>>>> 822f63bd43480db25cd87ea5f044bec0fbcfaa7a
+
         public AttributesParking(int numeroVagas)
         {
             NumeroVagas = numeroVagas;
+            Vagas = new List<bool>(new bool[NumeroVagas]);
         }
-<<<<<<< HEAD
-        public abstract void AlterarNumeroVagas(int novoNumero);
-        public virtual void ExibirNumeroVagas(Tipo_Veiculo tipo)
-        {
-            using (var query_vagas)
-=======
-        public virtual void AlterarNumeroVagas(int novoNumero) 
-        {
-            if (novoNumero > 0)
-            {
-                NumeroVagas = novoNumero;
-                Vagas = new List<bool>(new bool[NumeroVagas]);
-                Console.WriteLine($"Número de vagas ajustado para: {NumeroVagas}");
-            }
-            else
-            {
-                Console.WriteLine("O número de vagas deve ser maior que 0.");
-            }
-        }
+        public virtual void AlterarNumeroVagas(int novoNumero) { }
         public virtual void ExibirNumeroVagas(Tipo_Veiculo tipo)
         {
             var vagasDisp = Vagas.Select(x => x.Equals(true)).Count();
-            if (vagasDisp == 0)
->>>>>>> 822f63bd43480db25cd87ea5f044bec0fbcfaa7a
+            if (vagasDisp <= 0)
             {
                 Console.WriteLine($"\nNão há vagas disponíveis para {(tipo)} no momento.");
             }
