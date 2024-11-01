04bf3f507e3288c7d45bc2f188dafdfc6d0c5bcd
Paulo Arthur Araújo de Andrade <144744479+parthur207@users.noreply.github.com>
2024-10-31 22:06:50 -0300

    ok

--- Sistema de Estacionamento/DataBase/EF - CRUD/QueryCredential.cs
+++ Sistema de Estacionamento/DataBase/EF - CRUD/QueryCredential.cs
@@ -2,6 +2,7 @@
 using Sistema_de_Estacionamento.DataBase.Db_Context;
 using Sistema_de_Estacionamento.DataBase.EF;
 using Sistema_de_Estacionamento.DataBase.IEF___Interface;
+using Sistema_de_Estacionamento.Main;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +16,32 @@ namespace Sistema_de_Estacionamento.DataBase.EF___CRUD
     {
         public  AtributesClient dadosQuery_c { get; private set; }
         public AtributesVehicle dadosQuery_v { get; private set; }
+
+
+        public int Storage_Query()
+        {
+            Console.WriteLine("\nDigite o número correspondente a consulta:");
+            Console.WriteLine("1) Placa");
+            Console.WriteLine("2) Credencial");
+
+            if (!int.TryParse(Console.ReadLine(), out int numero) || numero<1 || numero>2)
+            {
+                Console.WriteLine("\nÉ necessário digitar um numero. Sendo 1 ou 2.");
+                Program.Main(ref_args);
+            }
+            else
+            {
+                if (numero == 1) {
+                    Console.WriteLine("\nDigite ");
+                }
+
+            }
+
+            return numero;
+        }
         public void QueryCredential_EF(string Credencial)
         {
-            
-
+           
             try {
                 using (var contextoQuery_Credential=new MyDbContext())
                 {
