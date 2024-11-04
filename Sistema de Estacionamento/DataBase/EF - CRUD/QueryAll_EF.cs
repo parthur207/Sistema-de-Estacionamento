7d9d54fe0e847595f3d9ae54ec85e471bb524bc7
Paulo Arthur Araújo de Andrade <144744479+parthur207@users.noreply.github.com>
2024-11-04 13:59:19 -0300

    ok

--- Sistema de Estacionamento/DataBase/EF - CRUD/QueryAll_EF.cs
+++ Sistema de Estacionamento/DataBase/EF - CRUD/QueryAll_EF.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-S
+
 namespace Sistema_de_Estacionamento.DataBase.EF___CRUD
 {
     internal class QueryAll_EF : IFeature_Parking
